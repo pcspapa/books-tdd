856d30a5f045082730f5f339f2299960a4039979
pcspapa <pcspapa@gmail.com>
2015-12-15 10:34:13 +0900

    5.2 passed

---
+++ src/main/java/com/cspark/books/tdd/Franc.java
@@ -0,0 +1,23 @@
+package com.cspark.books.tdd;
+
+/**
+ * Created by cspark on 2015. 12. 15..
+ */
+public class Franc {
+    public int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Franc dollar = (Franc) o;
+        return amount == dollar.amount;
+    }
+
+}
