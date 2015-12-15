4f929da7d3dff029e5993a778423099f0ad86bae
pcspapa <pcspapa@gmail.com>
2015-12-15 14:37:37 +0900

    14.1 failed

--- src/test/java/com/cspark/books/tdd/MoneyTest.java
+++ src/test/java/com/cspark/books/tdd/MoneyTest.java
@@ -73,4 +73,12 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferenceCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
