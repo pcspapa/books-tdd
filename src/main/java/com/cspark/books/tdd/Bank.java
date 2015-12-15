fd07ae7702fb1e82c4dd443583e73b9a971cdd4c
pcspapa <pcspapa@gmail.com>
2015-12-15 15:00:16 +0900

    14.4 failed

--- src/main/java/com/cspark/books/tdd/Bank.java
+++ src/main/java/com/cspark/books/tdd/Bank.java
@@ -1,17 +1,23 @@
 package com.cspark.books.tdd;
 
+import java.util.Hashtable;
+
 /**
  * Created by cspark on 2015. 12. 15..
  */
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 
     public int rate(String from, String to) {
-        return from.equals("CHF") && to.equals("USD") ? 2 : 1;
+        return ((Integer) rates.get(new Pair(from, to))).intValue();
     }
 }
---
+++ src/main/java/com/cspark/books/tdd/Pair.java
@@ -0,0 +1,26 @@
+package com.cspark.books.tdd;
+
+/**
+ * Created by cspark on 2015. 12. 15..
+ */
+public class Pair {
+    private String from;
+    private String to;
+
+    public Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Pair pair = (Pair) o;
+
+        return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
