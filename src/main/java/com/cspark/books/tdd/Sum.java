6ca8cd0d8bf402d40e5fd224f744fc416a65ccfa
pcspapa <pcspapa@gmail.com>
2015-12-15 14:14:29 +0900

    12.7 failed

--- src/main/java/com/cspark/books/tdd/Sum.java
+++ src/main/java/com/cspark/books/tdd/Sum.java
@@ -3,7 +3,11 @@ package com.cspark.books.tdd;
 /**
  * Created by cspark on 2015. 12. 15..
  */
-public class Sum {
+public class Sum implements Expression {
     public Money augend;
     public Money addend;
+
+    public Sum(Money augend, Money addend) {
+
+    }
 }
