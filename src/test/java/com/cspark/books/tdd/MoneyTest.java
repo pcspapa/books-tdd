ec9d8537af40d2b0694db5da0e1b042abd7c6dfc
pcspapa <pcspapa@gmail.com>
2015-12-15 11:14:46 +0900

    8.2 failed

--- src/test/java/com/cspark/books/tdd/MoneyTest.java
+++ src/test/java/com/cspark/books/tdd/MoneyTest.java
@@ -13,7 +13,7 @@ public class MoneyTest {
 
     @Test
     public void testDollarMuliplication() throws Exception {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
 
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
