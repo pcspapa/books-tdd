97b57dbc8479e4a361aebfe315431358efd63d1c
pcspapa <pcspapa@gmail.com>
2015-12-15 11:00:33 +0900

    5.3 refactoring

--- src/main/java/com/cspark/books/tdd/Dollar.java
+++ src/main/java/com/cspark/books/tdd/Dollar.java
@@ -13,10 +13,4 @@ public class Dollar extends Money {
         return new Dollar(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
 }
