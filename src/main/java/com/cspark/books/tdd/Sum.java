5f6c3d7ca36d4400e79c2725d96af4b4d3dad96c
pcspapa <pcspapa@gmail.com>
2015-12-15 15:28:03 +0900

    15.4 refactoring

--- src/main/java/com/cspark/books/tdd/Sum.java
+++ src/main/java/com/cspark/books/tdd/Sum.java
@@ -4,10 +4,10 @@ package com.cspark.books.tdd;
  * Created by cspark on 2015. 12. 15..
  */
 public class Sum implements Expression {
-    public Money augend;
-    public Money addend;
+    public Expression augend;
+    public Expression addend;
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
