e169f9e918518dfe9a2c6cb5296fc28543c385e5
pcspapa <pcspapa@gmail.com>
2015-12-15 11:18:31 +0900

    8.4 passed

--- src/main/java/com/cspark/books/tdd/Money.java
+++ src/main/java/com/cspark/books/tdd/Money.java
@@ -3,12 +3,14 @@ package com.cspark.books.tdd;
 /**
  * Created by cspark on 2015. 12. 15..
  */
-public class Money {
+public abstract class Money {
     protected int amount;
 
     static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
+
+    public abstract Money times(int amount);
 
     @Override
     public boolean equals(Object o) {
