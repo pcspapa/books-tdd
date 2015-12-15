7a040463e2ca319107918c77fe42c2fa0f25ecce
pcspapa <pcspapa@gmail.com>
2015-12-15 13:32:56 +0900

    10.1 refactoring

--- src/main/java/com/cspark/books/tdd/Dollar.java
+++ src/main/java/com/cspark/books/tdd/Dollar.java
@@ -10,7 +10,7 @@ public class Dollar extends Money {
     }
 
     public Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
+        return new Dollar(amount * multiplier, "USD");
     }
 
 }
