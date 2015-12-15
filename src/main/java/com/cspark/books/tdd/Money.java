641725c3d496a4b9687d2179804887c99a34fa5e
pcspapa <pcspapa@gmail.com>
2015-12-15 15:33:02 +0900

    15.5 refactoring

--- src/main/java/com/cspark/books/tdd/Money.java
+++ src/main/java/com/cspark/books/tdd/Money.java
@@ -24,11 +24,12 @@ public class Money implements Expression {
         return currency;
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
-    public Expression plus(Money addend) {
+    @Override
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
