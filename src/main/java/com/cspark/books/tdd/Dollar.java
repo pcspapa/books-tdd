04faff7560d0fe3abba493c0826ca9cae0efaa08
pcspapa <pcspapa@gmail.com>
2015-12-15 10:27:28 +0900

    3.4 passed

--- src/main/java/com/cspark/books/tdd/Dollar.java
+++ src/main/java/com/cspark/books/tdd/Dollar.java
@@ -16,7 +16,8 @@ public class Dollar {
 
     @Override
     public boolean equals(Object o) {
-        return true;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
     }
 
 }
