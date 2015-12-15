5549fb414f14b56fd37690079dc6e961f3288cfc
pcspapa <pcspapa@gmail.com>
2015-12-15 10:31:08 +0900

    4.2 refactoring

--- src/test/java/com/cspark/books/tdd/DollarTest.java
+++ src/test/java/com/cspark/books/tdd/DollarTest.java
@@ -14,11 +14,9 @@ public class DollarTest {
     @Test
     public void testMuliplication() throws Exception {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
 
-        product = five.times(3);
-        assertEquals(new Dollar(15), product);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
