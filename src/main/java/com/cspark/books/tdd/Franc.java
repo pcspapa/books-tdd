04711d1f4a3f798c222af5dae0d93c5393541074
pcspapa <pcspapa@gmail.com>
2015-12-15 11:01:54 +0900

    5.4 refactoring & passed

--- src/main/java/com/cspark/books/tdd/Franc.java
+++ src/main/java/com/cspark/books/tdd/Franc.java
@@ -4,7 +4,6 @@ package com.cspark.books.tdd;
  * Created by cspark on 2015. 12. 15..
  */
 public class Franc extends Money {
-    public int amount;
 
     public Franc(int amount) {
         this.amount = amount;
@@ -12,12 +11,6 @@ public class Franc extends Money {
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Franc dollar = (Franc) o;
-        return amount == dollar.amount;
     }
 
 }
--- src/test/java/com/cspark/books/tdd/MoneyTest.java
+++ src/test/java/com/cspark/books/tdd/MoneyTest.java
@@ -31,6 +31,9 @@ public class MoneyTest {
     public void testEquality() throws Exception {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 
 }
